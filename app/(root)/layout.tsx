7092520e8176410438f3520a75bb5364ce97ae47
33Frok1e <mratikrishna@gmail.com>
2025-02-24 16:12:28 +0530

    Right Sidebar Completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from '@/components/navigation/LeftSidebar'
 import Navbar from '@/components/navigation/navbar/Navbar'
+import RightSidebar from '@/components/navigation/RightSidebar'
 import React from 'react'
 
 const Layout = ({ children }: { children:React.ReactNode }) => {
@@ -8,12 +9,14 @@ const Layout = ({ children }: { children:React.ReactNode }) => {
         <Navbar />
         <div className='flex'>
             <LeftSidebar />
+
             <section className='min-h-screen flex flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
                 <div className='mx-auto w-full max-w-5xl'>
                     {children}
                 </div>
             </section>
-            {/* RightSidebar */}
+            
+            <RightSidebar />
         </div>
     </main>
   )
